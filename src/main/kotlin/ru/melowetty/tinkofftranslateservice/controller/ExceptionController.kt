8279fcb381f128d9d932cbde80ddc9ac571991e1
Denis Malinin <melowetty@mail.ru>
2024-08-02 21:07:48 +0500

    add exception handler

---
+++ src/main/kotlin/ru/melowetty/tinkofftranslateservice/controller/ExceptionController.kt
@@ -0,0 +1,14 @@
+package ru.melowetty.tinkofftranslateservice.controller
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.ErrorResponse
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+
+@ControllerAdvice
+class ExceptionController {
+    @ExceptionHandler(IllegalArgumentException::class)
+    fun handleIllegalArgumentException(e: IllegalArgumentException): ErrorResponse {
+        return ErrorResponse.create(e, HttpStatus.BAD_REQUEST, e.message!!)
+    }
+}
