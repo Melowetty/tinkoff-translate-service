c9b55a777d994262ae0826a3be0ac0e37d651c2a
Denis Malinin <melowetty@mail.ru>
2024-08-02 18:27:17 +0500

    initial commit

---
+++ src/main/kotlin/ru/melowetty/tinkofftranslateservice/TinkoffTranslateServiceApplication.kt
@@ -0,0 +1,11 @@
+package ru.melowetty.tinkofftranslateservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TinkoffTranslateServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TinkoffTranslateServiceApplication>(*args)
+}
---
+++ src/test/kotlin/ru/melowetty/tinkofftranslateservice/TinkoffTranslateServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.melowetty.tinkofftranslateservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TinkoffTranslateServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
