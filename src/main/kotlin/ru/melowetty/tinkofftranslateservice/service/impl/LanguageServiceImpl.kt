cded2a8f971b8470fb4ac5a257978a5234493f54
Denis Malinin <melowetty@mail.ru>
2024-08-04 03:09:00 +0500

    add language service impl

---
+++ src/main/kotlin/ru/melowetty/tinkofftranslateservice/service/impl/LanguageServiceImpl.kt
@@ -0,0 +1,27 @@
+package ru.melowetty.tinkofftranslateservice.service.impl
+
+import org.springframework.stereotype.Service
+import ru.melowetty.tinkofftranslateservice.entity.Language
+import ru.melowetty.tinkofftranslateservice.repository.LanguageRepository
+import ru.melowetty.tinkofftranslateservice.service.LanguageService
+
+@Service
+class LanguageServiceImpl(
+    private val languageRepository: LanguageRepository
+): LanguageService {
+    override fun getLanguages(): List<Language> {
+        return languageRepository.getAllLanguages()
+    }
+
+    override fun getLanguageByCode(code: String): Language? {
+        return languageRepository.getLanguageByCode(code)
+    }
+
+    override fun deleteLanguageByCode(code: String) {
+        return languageRepository.deleteLanguageByCode(code)
+    }
+
+    override fun createLanguage(code: String, name: String): Language {
+        return languageRepository.createLanguage(code, name)
+    }
+}
