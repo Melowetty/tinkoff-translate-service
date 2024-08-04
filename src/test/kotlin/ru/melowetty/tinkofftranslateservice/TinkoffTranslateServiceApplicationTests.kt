b533fe308d08cdb4e32c7a11f73fb5a03e5c565d
Denis Malinin <melowetty@mail.ru>
2024-08-05 02:47:42 +0500

    add prod props and add docker-compose file

--- src/test/kotlin/ru/melowetty/tinkofftranslateservice/TinkoffTranslateServiceApplicationTests.kt
+++ src/test/kotlin/ru/melowetty/tinkofftranslateservice/TinkoffTranslateServiceApplicationTests.kt
@@ -3,10 +3,8 @@ package ru.melowetty.tinkofftranslateservice
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
 class TinkoffTranslateServiceApplicationTests {
 
-    @Test
     fun contextLoads() {
     }
 
