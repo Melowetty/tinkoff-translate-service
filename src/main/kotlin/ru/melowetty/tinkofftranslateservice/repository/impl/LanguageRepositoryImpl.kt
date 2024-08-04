1df1480c91f5eeb14f3e17a4010fdb755bc8413e
Denis Malinin <melowetty@mail.ru>
2024-08-04 23:53:08 +0500

    fix language repository and language schema

--- src/main/kotlin/ru/melowetty/tinkofftranslateservice/repository/impl/LanguageRepositoryImpl.kt
+++ src/main/kotlin/ru/melowetty/tinkofftranslateservice/repository/impl/LanguageRepositoryImpl.kt
@@ -2,6 +2,8 @@ package ru.melowetty.tinkofftranslateservice.repository.impl
 
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.jdbc.core.queryForList
 import org.springframework.jdbc.support.GeneratedKeyHolder
 import org.springframework.stereotype.Repository
 import ru.melowetty.tinkofftranslateservice.entity.Language
@@ -22,7 +24,10 @@ class LanguageRepositoryImpl(
             psmt
 
         }, keyHolder)
-        val id = keyHolder.key?.toInt() ?:
+        val keys = keyHolder.keys ?:
+            throw RuntimeException("Попытка создать язык была неуспешной!")
+
+        val id = keys.get("id") as Int? ?:
             throw RuntimeException("Попытка создать язык была неуспешной!")
 
         return Language(
@@ -35,7 +40,16 @@ class LanguageRepositoryImpl(
     override fun getLanguageByCode(code: String): Language? {
         try {
             val sql = "SELECT * FROM language WHERE code = ?"
-            return jdbcTemplate.queryForObject(sql, Language::class.java, code.lowercase())
+            return jdbcTemplate.queryForObject(sql, RowMapper<Language> { rs, rowNum ->
+                val id = rs.getInt("id")
+                val name: String? = rs.getString("name")
+                val code = rs.getString("code")
+                Language(
+                    id = id,
+                    name = name,
+                    code = code,
+                )
+            }, code.lowercase())
         }
         catch (e: EmptyResultDataAccessException) {
             return null
@@ -48,6 +62,15 @@ class LanguageRepositoryImpl(
     }
 
     override fun getAllLanguages(): List<Language> {
-        return jdbcTemplate.queryForList("SELECT * FROM language ORDER BY id DESC", Language::class.java)
+        return jdbcTemplate.queryForList("SELECT * FROM language ORDER BY id ASC").map {
+            val id = it["id"]!! as Int
+            val name = it["name"] as String?
+            val code = it["code"]!! as String
+            Language(
+                id = id,
+                name = name,
+                code = code,
+            )
+        }
     }
 }
